8122a41175fe8001216e67b842bb63f6b27e572a
ROHANXI2 <m88231284@gmail.com>
2025-07-28 20:33:16 +0530

    Delete src directory

--- src/middleware/notFound.js
+++
@@ -1,8 +0,0 @@
-const notFound = (req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  error.statusCode = 404;
-  error.isOperational = true;
-  next(error);
-};
-
-module.exports = notFound;
